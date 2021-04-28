bc55eaf060e35ad23e40e5ccf7f70b224d44c507
Shahriyar Aghajani <shahriyar.a13@gmail.com>
2021-04-29 00:04:30 +0430

    v2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -54,8 +54,8 @@ object AppConfig {
     const val compileSdk = 30
     const val minSdk = 21
     const val targetSdk = 30
-    const val versionCode = 1
-    const val versionName = "1.0.0"
+    const val versionCode = 2
+    const val versionName = "1.0.1"
     const val buildToolsVersion = "30.0.3"
 
     const val androidTestInstrumentation = "androidx.test.runner.AndroidJUnitRunner"
